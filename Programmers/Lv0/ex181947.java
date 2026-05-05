f3dfb2489eaf0386b1019e4ea5ddbb55d728eaa3
Silver_Eun <h22sang@naver.com>
2026-05-05 18:48:27 +0900

    solve(Lv0): 덧셈식 출력하기

---
+++ Programmers/Lv0/ex181947.java
@@ -0,0 +1,15 @@
+package Lv0;
+
+import java.util.Scanner;
+
+public class ex181947 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+
+}
