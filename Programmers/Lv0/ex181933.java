4ce3e68636cebe295ba502ae70d7ce30bfb161a2
Silver_Eun <h22sang@naver.com>
2026-05-11 20:30:35 +0900

    solve(Lv0): 코드 처리하기

--- Programmers/Lv0/ex181933.java
+++ Programmers/Lv0/ex181933.java
@@ -7,25 +7,39 @@ public class ex181933 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        boolean flag = sc.nextBoolean();
+        String s = sc.nextLine();
 
         ex181933 outer = new ex181933();
         ex181933.Solution sol = outer.new Solution();
 
-        int result = sol.solution(a, b, flag);
+        String result = sol.solution(s);
         System.out.println(result);
 
         sc.close();
     }
 
     class Solution {
-        public int solution(int a, int b, boolean flag) {
-            int answer = 0;
+        public String solution(String code) {
+            String answer = "";
+            int mode = 0;
 
-            if (flag) answer = a + b;
-            else answer = a - b;
+            for (int i = 0; i < code.length(); i++) {
+                char c = code.charAt(i);
+
+                if (c == '1') {
+                    mode = 1 - mode;
+                } else {
+                    if (mode == 0 && i % 2 == 0) {
+                        answer += c;
+                    } else if (mode == 1 && i % 2 == 1) {
+                        answer += c;
+                    }
+                }
+            }
+
+            if (answer.equals("")) {
+                return "EMPTY";
+            }
 
             return answer;
         }
