c75952a891439ae044fa654e578a5ab65344d358
Silver_Eun <h22sang@naver.com>
2026-05-15 23:54:53 +0900

    solve(Lv0): 수열과 구간 쿼리3

---
+++ Programmers/Lv0/ex181924.java
@@ -0,0 +1,52 @@
+package Lv0;
+
+import java.util.Scanner;
+
+public class ex181924 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int q = sc.nextInt();
+        int[][] queries = new int[q][2];
+
+        for (int i = 0; i < q; i++) {
+            queries[i][0] = sc.nextInt();
+            queries[i][1] = sc.nextInt();
+        }
+
+        ex181924 outer = new ex181924();
+        Solution sol = outer.new Solution();
+
+        int[] result = sol.solution(arr, queries);
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+
+        sc.close();
+    }
+
+    class Solution {
+        public int[] solution(int[] arr, int[][] queries) {
+
+            for (int i = 0; i < queries.length; i++) {
+                int a = queries[i][0];
+                int b = queries[i][1];
+
+                int temp = arr[a];
+                arr[a] = arr[b];
+                arr[b] = temp;
+            }
+
+            return arr;
+        }
+    }
+}
