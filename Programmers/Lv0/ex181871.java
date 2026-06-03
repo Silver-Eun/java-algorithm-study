6b85ce3b5bb686eaf81606bd5c3935d078eb560b
Silver_Eun <h22sang@naver.com>
2026-06-03 17:29:03 +0900

    solve(Lv0): 문자열이 몇 번 등장하는지 세기

---
+++ Programmers/Lv0/ex181871.java
@@ -0,0 +1,36 @@
+package Lv0;
+
+import java.util.Scanner;
+
+public class ex181871 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String c = sc.nextLine();
+
+        ex181871 outer = new ex181871();
+        Solution sol = outer.new Solution();
+
+        int result = sol.solution(s, c);
+
+        System.out.println(result);
+
+        sc.close();
+
+        sc.close();
+    }
+
+    class Solution {
+        public int solution(String myString, String pat) {
+            int answer = 0;
+
+            for (int i = 0; i <= myString.length() - pat.length(); i++) {
+                if (myString.substring(i, i + pat.length()).equals(pat))
+                    answer++;
+            }
+
+            return answer;
+        }
+    }
+}
