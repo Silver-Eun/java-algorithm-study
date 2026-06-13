a32b3df65896cadb20fa35f07466282a5f69181d
Silver_Eun <h22sang@naver.com>
2026-06-13 23:59:23 +0900

    solve(Lv0): 배열의 원소 삭제하기

---
+++ Programmers/Lv0/ex181844.java
@@ -0,0 +1,20 @@
+package Lv0;
+
+import java.util.Scanner;
+
+public class ex181844 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        sc.close();
+    }
+
+    class Solution {
+        public int[] solution(int[] arr, int[] delete_list) {
+            int[] answer = {};
+            return answer;
+        }
+    }
+}
