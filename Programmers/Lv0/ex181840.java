d13d2cd72519f16892f94c686600b87e68d2f254
Silver_Eun <h22sang@naver.com>
2026-06-15 16:59:29 +0900

    solve(Lv0): 정수 찾기

---
+++ Programmers/Lv0/ex181840.java
@@ -0,0 +1,43 @@
+package Lv0;
+
+import java.util.Scanner;
+
+public class ex181840 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] int_list = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int_list[i] = sc.nextInt();
+        }
+
+        int ex = sc.nextInt();
+
+        ex181840 outer = new ex181840();
+        Solution sol = outer.new Solution();
+
+       int result = sol.solution(int_list, ex);
+
+        System.out.println(result);
+
+        sc.close();
+    }
+
+    class Solution {
+        public int solution(int[] num_list, int n) {
+            int answer = 0;
+
+            for(int i = 0; i < num_list.length; i++) {
+                if(num_list[i] == n) {
+                    answer = 1;
+                    break;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
