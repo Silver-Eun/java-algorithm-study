8651d63f6c199ce4e9bd6c4d030b9667b2526f97
Silver_Eun <h22sang@naver.com>
2026-07-06 17:45:52 +0900

    solve(Lv0): 숨어있는 숫자의 덧셈 (1)

---
+++ Programmers/Lv0/ex120851.java
@@ -0,0 +1,34 @@
+package Lv0;
+
+import java.util.Scanner;
+
+public class ex120851 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        ex120851 outer = new ex120851();
+        Solution sol = outer.new Solution();
+
+        int[] result = sol.solution(n);
+
+        System.out.println(result);
+
+        sc.close();
+    }
+
+    class Solution {
+        public int solution(String my_string) {
+            int answer = 0;
+
+            for (int i = 0; i < my_string.length(); i++) {
+                if (Character.isDigit(my_string.charAt(i)))
+                    answer += my_string.charAt(i) - '0';
+            }
+
+            return answer;
+        }
+    }
+}
