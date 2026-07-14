aa36fa414e47d3676f92b2ebffda3a874905d2a0
Silver_Eun <h22sang@naver.com>
2026-07-14 19:29:52 +0900

    solve(Lv0): 문자 반복 출력하기

---
+++ Programmers/Lv0/ex120825.java
@@ -0,0 +1,35 @@
+package Lv0;
+
+import java.util.Scanner;
+
+public class ex120825 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        int n = sc.nextInt();
+
+        ex120825 outer = new ex120825();
+        Solution sol = outer.new Solution();
+
+        String result = sol.solution(a, b);
+
+        System.out.println(result);
+
+        sc.close();
+    }
+
+    class Solution {
+        public String solution(String my_string, int n) {
+            String answer = "";
+
+            for (int i = 0; i < my_string.length(); i++) {
+                for (int j = 0; j < n; j++)
+                    answer += my_string.charAt(i);
+            }
+
+            return answer;
+        }
+    }
+}
