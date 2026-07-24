25517b55c52bc9aa9b4fa66940b59f97b5f8140c
Silver_Eun <h22sang@naver.com>
2026-07-24 21:24:42 +0900

    solve(Lv0): 자릿수 더하기

---
+++ Programmers/Lv0/ex012931.java
@@ -0,0 +1,35 @@
+package Lv0;
+
+import java.util.Scanner;
+
+public class ex012931 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        ex012931 outer = new ex012931();
+        Solution sol = outer.new Solution();
+
+        long result = sol.solution(n);
+
+        System.out.println(result);
+
+        sc.close();
+    }
+
+    public class Solution {
+        public int solution(int n) {
+            int answer = 0;
+
+            String s = String.valueOf(n);
+
+            for(int i = 0; i < s.length(); i++) {
+                answer += s.charAt(i) - '0';
+            }
+
+            return answer;
+        }
+    }
+}
