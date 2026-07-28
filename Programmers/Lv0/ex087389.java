f78b972d1ae42afe219789fdb2f67c8b3ebb6d05
Silver_Eun <h22sang@naver.com>
2026-07-28 21:46:42 +0900

    solve(Lv0): 나머지가 1이 되는 수 찾기

---
+++ Programmers/Lv0/ex087389.java
@@ -0,0 +1,36 @@
+package Lv0;
+
+import java.util.Scanner;
+
+public class ex087389 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        ex087389 outer = new ex087389();
+        Solution sol = outer.new Solution();
+
+        int result = sol.solution(n);
+
+        System.out.println(result);
+
+        sc.close();
+    }
+
+    class Solution {
+        public int solution(int n) {
+            int answer = 0;
+
+            for (int i = 1; i <= Integer.MAX_VALUE; i++) {
+                if (n % i == 1) {
+                    answer = i;
+                    break;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
