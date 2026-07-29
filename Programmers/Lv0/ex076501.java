123eba68647ef59f0af0a90f3b8bc04ce85153b1
Silver_Eun <h22sang@naver.com>
2026-07-29 23:51:23 +0900

    solve(Lv1): 없는 숫자 더하기

---
+++ Programmers/Lv0/ex076501.java
@@ -0,0 +1,48 @@
+package Lv0;
+
+import java.util.Scanner;
+
+public class ex076501 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] absolutes = new int[n];
+        boolean[] signs = new boolean[n];
+
+
+        for (int i = 0; i < n; i++) {
+            absolutes[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            signs[i] = sc.nextBoolean();
+        }
+
+        ex076501 outer = new ex076501();
+        Solution sol = outer.new Solution();
+
+        int result = sol.solution(absolutes, signs);
+
+        System.out.println(result);
+
+        sc.close();
+    }
+
+    class Solution {
+        public int solution(int[] absolutes, boolean[] signs) {
+            int answer = 0;
+
+            for (int i = 0; i < absolutes.length; i++) {
+                if (!signs[i])
+                    answer += -absolutes[i];
+                else
+                    answer += absolutes[i];
+            }
+
+            return answer;
+        }
+    }
+}
