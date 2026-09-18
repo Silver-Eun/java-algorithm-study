f495e1ce61faed4542b09fcb299b343786b1f645
Silver_Eun <h22sang@naver.com>
2026-09-19 00:34:30 +0900

    solve(Lv2): 방문 길이

---
+++ Programmers/Lv0/ex049994.java
@@ -0,0 +1,60 @@
+package Lv0;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class ex049994 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        ex049994 outer = new ex049994();
+        Solution sol = outer.new Solution();
+
+        int result = sol.solution(s);
+
+        System.out.println(result);
+
+        sc.close();
+    }
+
+    class Solution {
+        public int solution(String dirs) {
+            int answer = 0;
+            int x = 0;
+            int y = 0;
+
+            HashSet<String> set = new HashSet<>();
+
+            for (char dir : dirs.toCharArray()) {
+                int nx = x;
+                int ny = y;
+
+                if (dir == 'U') ny++;
+                else if (dir == 'D') ny--;
+                else if (dir == 'R') nx++;
+                else if (dir == 'L') nx--;
+
+                if (nx < -5 || nx > 5 || ny < -5 || ny > 5) {
+                    continue;
+                }
+
+                String road1 = x + "," + y + "-" + nx + "," + ny;
+                String road2 = nx + "," + ny + "-" + x + "," + y;
+
+                if (!set.contains(road1)) {
+                    answer++;
+                    set.add(road1);
+                    set.add(road2);
+                }
+
+                x = nx;
+                y = ny;
+            }
+
+            return answer;
+        }
+    }
+}
